f940caf64673726d47d95edbb46b3c89e7f871b4
Steffen <dickst@mathematik.uni-marburg.de>
2024-10-09 22:55:58 +0200

    removed unused variable

--- lib/gui/Screens/mainScreen.dart
+++ lib/gui/Screens/mainScreen.dart
@@ -18,8 +18,6 @@ class MainScreen extends StatefulWidget {
 
 class _MainScreenState extends State<MainScreen> {
   int _selectedIndex = 0;
-  static const TextStyle optionStyle =
-  TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: textColor);
 
   void _onItemTapped(int index) {
     setState(() {
