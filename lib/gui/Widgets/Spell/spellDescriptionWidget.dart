ec5882ae209fc0b326e12cb37a1f9f64aac8e0f9
Steffen <dickst@mathematik.uni-marburg.de>
2024-10-20 00:14:27 +0200

    fixed tables overflowing

--- lib/gui/Widgets/Spell/spellDescriptionWidget.dart
+++ lib/gui/Widgets/Spell/spellDescriptionWidget.dart
@@ -11,6 +11,44 @@ class SpellDescriptionWidget extends StatelessWidget {
   final List<EntryHigherLevel> atHigherLevels;
   const SpellDescriptionWidget(this.description, this.atHigherLevels,{super.key});
 
+  Widget _renderMarkdown(String s){
+    if(s.contains("| ----------- | ----------- |")){
+      int first = s.indexOf("|");
+      String header = s.substring(0,first).replaceAll("\n", "");
+      String content = s.substring(first);
+      return Container(
+        margin: const EdgeInsets.only(top: 10),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            MarkdownBody(
+              data: FiveEToolsConverter.translateAnnotations(header),
+              styleSheet: markDownStyle,
+            ),
+            const SizedBox(
+              height: 2,
+            ),
+            SingleChildScrollView(
+              physics: const ClampingScrollPhysics(),
+              scrollDirection: Axis.horizontal,
+              child: MarkdownBody(
+                data: FiveEToolsConverter.translateAnnotations(content),
+                styleSheet: markDownStyle,
+              ),
+                ),
+          ],
+        ),
+      );
+    }
+    return Container(
+          margin: const EdgeInsets.only(top: 10),
+          child: MarkdownBody(
+            data: FiveEToolsConverter.translateAnnotations(s),
+            styleSheet: markDownStyle,
+          ),
+        );
+  }
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -24,15 +62,7 @@ class SpellDescriptionWidget extends StatelessWidget {
     List<Widget> children = [];
     for(int i = 0; i <description.length;i++){
       String s = description[i];
-      children.add(
-          Container(
-            margin: EdgeInsets.only(bottom: i < description.length -1 ? 10: 0,),
-            child: MarkdownBody(
-                data: FiveEToolsConverter.translateAnnotations(s),
-                styleSheet: markDownStyle,
-            ),
-          )
-      );
+      children.add(_renderMarkdown(s));
     }
     if(atHigherLevels.isNotEmpty){
       children.add(
@@ -45,15 +75,7 @@ class SpellDescriptionWidget extends StatelessWidget {
       );
       for(EntryHigherLevel e in atHigherLevels){
         for(String s in e.entries) {
-          children.add(
-              Container(
-                margin: const EdgeInsets.only(top: 10),
-                child: MarkdownBody(
-                  data: FiveEToolsConverter.translateAnnotations(s),
-                  styleSheet: markDownStyle,
-                ),
-              )
-          );
+          children.add(_renderMarkdown(s));
         }
       }
     }
