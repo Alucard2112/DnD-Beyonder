91e3392b048b4c5df5b12008d04fad52266c2c7b
Steffen <dickst@mathematik.uni-marburg.de>
2024-11-10 15:34:58 +0100

    improved translation

--- lib/converters/fiveEToolsConverter.dart
+++ lib/converters/fiveEToolsConverter.dart
@@ -53,56 +53,80 @@ class FiveEToolsConverter {
 
   static String translateAnnotations(String s){
     String ret = s;
-    if(ret.contains("{@dice")||ret.contains("{@damage")){
-      ret = _translateDamage(ret);
-    }
-    if(ret.contains("{@condition")){
-      ret = _translateCondition(ret);
-    }
-    if(ret.contains("{@creature")){
-      ret = _translateCreature(ret);
-    }
-    if(ret.contains("{@hit")){
-      ret = _translateHit(ret);
-    }
-    if(ret.contains("{@spell")){
-      ret = _translateSpell(ret);
-    }
-    if(ret.contains("{@sense")){
-      ret = _translateSenses(ret);
-    }
-    if(ret.contains("{@d20")){
-      ret = _translated20(ret);
-    }
-    if(ret.contains("{@action")){
-      ret = _translateAction(ret);
-    }
-    if(ret.contains("{@skill")){
-      ret = _translateSkills(ret);
-    }
-    if(ret.contains("{@race")){
-      ret = _translateSpecies(ret);
-    }
-    if(ret.contains("{@status")){
-      ret = _translateStatus(ret);
-    }
-    if(ret.contains("{@quickref")){
-      ret = _translateQuickref(ret);
-    }
-    if(ret.contains("{@item")){
-      ret = _translateItems(ret);
-    }
-    if(ret.contains("{@scale")){
-      ret = _translateScaledamage(ret);
-    }
-    if(ret.contains("@filter")){
-      ret = _translateFilter(ret);
+    if(ret.contains("{@")){
+      ret = _translateGeneric(ret);
     }
     if(ret.contains("{@note")){
       ret = _translateNote(ret);
     }
-    if(ret.contains("{@")){
-      ret = _translateGeneric(ret);
+    return ret;
+  }
+
+  static String _translateGeneric(String s){
+    String ret = s;
+    RegExp exp = RegExp(r'{@[^}]+}'); //RegExp(r'{@[0-9a-zA-Z| \(\)\[\];&]+}');
+    Iterable<Match> matches = exp.allMatches(s);
+    for (final Match m in matches) {
+      String match = m[0]!;
+      String start = match.substring(match.indexOf("@")+1, match.indexOf(" "));
+      String replace = match;
+      switch(start){
+        case "dice":
+          replace = _translateDamage(match);
+          break;
+        case "damage":
+          replace = _translateDamage(match);
+          break;
+        case "condition":
+          replace = _translateCondition(match);
+          break;
+        case "creature":
+          replace = _translateCreature(match);
+          break;
+        case "hit":
+          replace = _translateHit(match);
+          break;
+        case "spell":
+          replace = _translateSpell(match);
+          break;
+        case "sense":
+          replace = _translateSenses(match);
+          break;
+        case "d20":
+          replace = _translated20(match);
+          break;
+        case "action":
+          replace = _translateAction(match);
+          break;
+        case "skill":
+          replace = _translateSkills(match);
+          break;
+        case "race":
+          replace = _translateSpecies(match);
+          break;
+        case "status":
+          replace = _translateStatus(match);
+          break;
+        case "quickref":
+          replace = _translateQuickref(match);
+          break;
+        case "item":
+          replace = _translateItems(match);
+          break;
+        case "scaledamage":
+          replace = _translateScaledamage(match);
+          break;
+        case "filter":
+          replace = _translateFilter(match);
+          break;
+        default:
+          replace = match.substring(match.indexOf(" ")+1).replaceAll("}", "");
+          if(replace.contains("|")){
+            replace = replace.substring(replace.lastIndexOf("|"));
+          }
+          replace = replace.substring(0, replace.indexOf("|"));
+      }
+      ret = ret.replaceAll(match, replace);
     }
     return ret;
   }
@@ -120,70 +144,29 @@ class FiveEToolsConverter {
   }
 
   static String _translateFilter(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@filter [0-9a-zA-Z| \[\];&]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.substring(match.indexOf(" ")+1).replaceAll("}", "");
-      if(hit.contains("|")){
-        hit = hit.substring(0,hit.indexOf("|"));
-      }
-      ret = ret.replaceAll(match, "*$hit*");
+    String hit = s.substring(s.indexOf(" ")+1).replaceAll("}", "");
+    if(hit.contains("|")){
+      hit = hit.substring(0,hit.indexOf("|"));
     }
-    return ret;
+    return hit;
   }
 
   static String _translateScaledamage(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@scale[a-zA-Z]+ [0-9a-zA-Z| ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("}", "");
-      String dieString = hit.substring(hit.lastIndexOf("|"));
-      List<String> split = dieString.split("d");
-      int type = int.parse(split[1].trim());
-      int amount = int.parse(split[0].trim());
-      ret = ret.replaceAll(match, _translateDiceHelper(amount, type));
-    }
-    return ret;
-  }
-
-  static String _translateGeneric(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@[0-9a-zA-Z| ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.substring(match.indexOf(" ")+1).replaceAll("}", "");
-      if(hit.contains("|")){
-        hit = hit.substring(hit.lastIndexOf("|"));
-      }
-      ret = ret.replaceAll(match, hit.substring(0, hit.indexOf("|")));
-    }
-    return ret;
+    String hit = s.replaceAll("}", "");
+    String dieString = hit.substring(hit.lastIndexOf("|"));
+    List<String> split = dieString.split("d");
+    int type = int.parse(split[1].trim());
+    int amount = int.parse(split[0].trim());
+    return _translateDiceHelper(amount, type);
   }
 
   static String _translateItems(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@item [0-9a-zA-Z| ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@item ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, "*${hit.substring(hit.lastIndexOf("|"))}*");
-    }
-    return ret;
+      String hit = s.replaceAll("{@item ", "").replaceAll("}", "");
+      return hit.substring(hit.lastIndexOf("|"));
   }
 
   static String _translateQuickref(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@quickref [0-9a-zA-Z| ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@quickref ", "").replaceAll("}", "");
+      String hit = s.replaceAll("{@quickref ", "").replaceAll("}", "");
       String replace = "";
       if(hit.toLowerCase().contains("cover")){
         replace = S.current.cover(hit.substring(hit.lastIndexOf("|")+1).toLowerCase().trim().replaceAll(RegExp(r"\W"), "_"));
@@ -194,141 +177,83 @@ class FiveEToolsConverter {
       else{
         replace = S.current.difficultTerrain;
       }
-      ret = ret.replaceAll(match, replace);
-    }
-    return ret;
+      return replace;
   }
 
   static String _translateStatus(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@status [a-zA-Z|]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@status ", "").replaceAll("}", "");
+      String hit = s.replaceAll("{@status ", "").replaceAll("}", "");
       if(hit.contains("|")){
         hit = hit.substring(0, hit.indexOf("|"));
       }
-      ret = ret.replaceAll(match, "*${S.current.status(hit.toLowerCase().trim().replaceAll(RegExp(r"\W"), "_"))}*");
-    }
-    return ret;
+      return "*${S.current.status(hit.toLowerCase().trim().replaceAll(RegExp(r"\W"), "_"))}*";
   }
 
   static String _translateSpecies(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@race [a-zA-Z]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@race ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, "*${S.current.species(hit.toLowerCase().trim().replaceAll(RegExp(r"\W"), "_"))}*");
-    }
-    return ret;
+      String hit = s.replaceAll("{@race ", "").replaceAll("}", "");
+      if(hit.contains("|")){
+        hit = hit.substring(0,hit.indexOf("|"));
+        hit = hit.replaceAll(" ", "");
+      }
+      String speciesString = hit.toLowerCase().trim().replaceAll(RegExp(r"\W"), "_");
+      while(speciesString.endsWith("_")){
+        speciesString = speciesString.substring(0, hit.length-1);
+      }
+      print(speciesString);
+      return "*${S.current.species(speciesString)}*";
   }
 
   static String _translateSkills(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@skill [a-zA-Z]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@skill ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, "*${S.current.skills(hit.toLowerCase().trim().replaceAll(" ", "_"))}*");
-    }
-    return ret;
+      String hit = s.replaceAll("{@skill ", "").replaceAll("}", "");
+      return "*${S.current.skills(hit.toLowerCase().trim().replaceAll(" ", "_"))}*";
   }
 
   static String _translateSenses(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@sense [a-zA-Z]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@sense ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, "*${S.current.senses(hit.toLowerCase().trim().replaceAll(" ", "_"))}*");
-    }
-    return ret;
+      String hit = s.replaceAll("{@sense ", "").replaceAll("}", "");
+      return "*${S.current.senses(hit.toLowerCase().trim().replaceAll(" ", "_"))}*";
   }
 
   static String _translateAction(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@action [a-zA-Z ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@action ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, S.current.actions(hit.toLowerCase().trim().replaceAll(" ", "_")));
-    }
-    return ret;
+      String hit = s.replaceAll("{@action ", "").replaceAll("}", "");
+      return S.current.actions(hit.toLowerCase().trim().replaceAll(" ", "_"));
   }
 
   static String _translated20(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@d20 [0-9]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@d20 ", "+").replaceAll("}", "");
-      ret = ret.replaceAll(match, hit);
-    }
-    return ret;
+      return s.replaceAll("{@d20 ", "+").replaceAll("}", "");
   }
 
   static String _translateSpell(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@spell [0-9a-zA-Z ]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@spell ", "").replaceAll("}", "");
+      String hit = s.replaceAll("{@spell ", "").replaceAll("}", "");
       List<Spell> spells = BoxHandler.spellBox.values.where((Spell s)=>s.name["en"]!.toLowerCase().trim()==hit.toLowerCase().trim()).toList();
-      ret = ret.replaceAll(match, "*${spells.isEmpty ? hit : spells[0].getName()}*");
-    }
-    return ret;
+      return "*${spells.isEmpty ? hit : spells[0].getName()}*";
   }
 
   static String _translateHit(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@hit \+[0-9]+}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      String hit = match.replaceAll("{@hit ", "").replaceAll("}", "");
-      ret = ret.replaceAll(match, hit);
-    }
-    return ret;
+      return s.replaceAll("{@hit ", "").replaceAll("}", "");
   }
 
   static String _translateCreature(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@creature [a-zA-Z |()]*}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      if(match.contains("huge")) {
-        ret = ret.replaceAll(match, S.current.huge);
+      if(s.contains("huge")) {
+        return S.current.huge;
       }
-      else if(match.contains("tiny")){
-        ret = ret.replaceAll(match, S.current.tiny);
+      else if(s.contains("tiny")){
+        return S.current.tiny;
       }
-      else if(match.contains("small")){
-        ret = ret.replaceAll(match, S.current.small);
+      else if(s.contains("small")){
+        return S.current.small;
       }
-      else if(match.contains("medium")){
-        ret = ret.replaceAll(match, S.current.medium);
+      else if(s.contains("medium")){
+        return S.current.medium;
       }
-      else if(match.contains("large")){
-        ret = ret.replaceAll(match, S.current.large);
+      else if(s.contains("large")){
+        return S.current.large;
       }
       else{
-        String creature = match.replaceAll("{@creature ", "").replaceAll("}", "");
+        String creature = s.replaceAll("{@creature ", "").replaceAll("}", "");
         if(creature.contains("|")){
           creature = creature.substring(creature.lastIndexOf("|")+1);
         }
-        ret = ret.replaceAll(match, "*$creature*");
+        return "*$creature*";
       }
-    }
-    return ret;
   }
 
   static String _translateCondition(String s){
@@ -339,20 +264,15 @@ class FiveEToolsConverter {
       String match = m[0]!;
       String condition = match.replaceAll("{@condition ", "").replaceAll("}", "");
       if(condition.contains("|")){
-        condition = condition.substring(0,condition.indexOf("|"));
+        return condition.substring(condition.lastIndexOf("|")+1);
       }
-      ret = ret.replaceAll(match, S.current.condition(condition));
+      return S.current.condition(condition);
     }
     return ret;
   }
 
   static String _translateDamage(String s){
-    String ret = s;
-    RegExp exp = RegExp(r'{@dice [0-9]*d[0-9]+( [+\-×] [0-9]+)?(\|[0-9]+)?}|{@damage [0-9]*d[0-9]+( [+\-×] [0-9]+)?(\|[0-9]+)?}');
-    Iterable<Match> matches = exp.allMatches(s);
-    for (final Match m in matches) {
-      String match = m[0]!;
-      List<String> dieString = match.replaceAll("{@dice ", "").replaceAll("{@damage ", "").replaceAll("}", "").split("d");
+      List<String> dieString = s.replaceAll("{@dice ", "").replaceAll("{@damage ", "").replaceAll("}", "").split("d");
       int? amount;
       if(dieString[0].isNotEmpty){
         amount = int.parse(dieString[0]);
@@ -387,9 +307,7 @@ class FiveEToolsConverter {
           type = int.parse(dieString[1]);
         }
       }
-      ret = ret.replaceAll(match, "*$before${_translateDiceHelper(amount, type)}$add$after*");
-    }
-    return ret;
+      return "*$before${_translateDiceHelper(amount, type)}$add$after*";
   }
 
   static String _translateDiceHelper(int? amount, int type){
